9dd3cd3e029d06bae3579026042519cd03605932
LeoPisan <webtools.trough480@passinbox.com>
2025-05-22 14:45:13 +0200

    refactor(Outils-Developpement): séparation des infos d'implémentation de celles du toolkit dans le logger
    On en profite pour refaire la documentation des niveaux de log

--- Outils-Developpement/Logging/NiveauxLog.cs
+++ Outils-Developpement/Logging/NiveauxLog.cs
@@ -2,8 +2,24 @@ namespace Outils_Developpement.Logging;
 
 public enum NiveauxLog
 {
-    Info, // Informations générales
-    Action, // Actions plus importantes que de simples informations
-    Erreur, // Erreurs
-    None // Aucun niveau de log, ne doit pas être utilisé pour signaler un message mais uniquement comme niveau du logger
+    /// <summary>
+    /// Informations générales concernant le toolkit
+    /// </summary>
+    InfoToolkit,
+    /// <summary>
+    /// Informations générales
+    /// </summary>
+    Info,
+    /// <summary>
+    /// Actions plus importantes que de simples informations
+    /// </summary>
+    Action,
+    /// <summary>
+    /// Erreurs
+    /// </summary>
+    Erreur,
+    /// <summary>
+    /// Aucun niveau de log, ne doit pas être utilisé pour signaler un message mais uniquement comme niveau du logger
+    /// </summary>
+    None
 }
